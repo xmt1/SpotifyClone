8e94c4f05a8e9ffc8f4058a93eac78290eb7a782
Xavier Trinidad <xmtrinidad2@gmail.com>
2018-12-30 18:12:31 -0700

    Added validation messages

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php 
   include('includes/classes/Account.php');
   $account = new Account();
-  $account->register();
+  
   include('includes/handlers/register-handler.php');
   include('includes/handlers/login-handler.php');
 ?>
@@ -33,18 +33,23 @@
     <form id="registerForm" action="register.php" method="POST">
       <h2>Create your free account</h2>
       <div>
+        <?php echo $account->getError("Your username must be between 5 and 25 characters"); ?>
         <label for="username">Username</label>
         <input type="text" id="username" name="username" required>
       </div>
       <div>
+        <?php echo $account->getError("Your first name must be between 2 and 25 characters"); ?>
         <label for="firstName">First Name</label>
         <input type="text" id="firstName" name="firstName" required>
       </div>
       <div>
+        <?php echo $account->getError("Your last name must be between 2 and 25 characters"); ?>
         <label for="lastName">Last Name</label>
         <input type="text" id="lastName" name="lastName" required>
       </div>
       <div>
+        <?php echo $account->getError("Your emails don't match"); ?>
+        <?php echo $account->getError("Email is invalid"); ?>
         <label for="email">Email</label>
         <input type="email" id="email" name="email" required>
       </div>
@@ -54,6 +59,9 @@
       </div>
 
       <div>
+        <?php echo $account->getError("Your passwords don't match"); ?>
+        <?php echo $account->getError("Your passwords can only contain numbers and letters"); ?>
+        <?php echo $account->getError("Your password must be between 5 and 30 characters"); ?>
         <label for="password">Password</label>
         <input type="password" id="password" name="password" required>
       </div>
